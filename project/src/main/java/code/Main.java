a31b3e9cc1fbbd22df124f3274892f28a86fec60
theslatcher <zzoomm-game@hotmail.com>
2021-05-08 14:53:07 +0200

    Added missing s in Views.

--- project/src/main/java/code/Main.java
+++ project/src/main/java/code/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("View/login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Views/login.fxml"));
         primaryStage.setTitle("Group 9");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
